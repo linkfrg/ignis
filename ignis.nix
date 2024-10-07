fe901601c1833ba81e1241df64f39b4ecda9adb0
Grigory <154010620+somokill@users.noreply.github.com>
2024-10-07 20:10:11 +0500

    Add derivation

---
+++ ignis.nix
@@ -0,0 +1,99 @@
+{ pkgs, ... }:
+let
+inherit (pkgs.lib) concatStringsSep;
+in
+pkgs.stdenv.mkDerivation {
+  pname = "ignis";
+  version = "0.2";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "linkfrg";
+    repo = "ignis";
+    rev = "78846c57832c70fd971dbcd28ac5d8e70cd09840";
+    sha256 = "sha256-V70DT1Czyj/rbBAQIqfRVlQOyeuLjk1F6P8XOagDxNw=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    pkgs.pkg-config
+    pkgs.meson
+    pkgs.ninja
+    pkgs.git
+    pkgs.makeWrapper
+  ];
+
+  buildInputs = [
+    pkgs.glib
+    pkgs.gtk4
+    pkgs.gtk4-layer-shell
+    pkgs.libpulseaudio
+    pkgs.python312Packages.pygobject3
+    pkgs.python312Packages.pycairo
+    pkgs.python312Packages.click
+    pkgs.python312Packages.gst-python
+    pkgs.python312Packages.charset-normalizer
+  ];
+
+  runtimeInputs = [
+    pkgs.gst_all_1.gstreamer
+    pkgs.gst_all_1.gst-plugins-base
+    pkgs.dart-sass
+  ];
+
+  patchPhase = ''
+    substituteInPlace ignis/utils/sass.py \
+      --replace-fail '/bin/sass' '${pkgs.dart-sass}/bin/sass'
+  '';
+
+  buildPhase = ''
+    cd ..
+    meson setup build --prefix=$out --libdir=lib/ignis
+    ninja -C build
+  '';
+
+  installPhase = ''
+    ninja -C build install
+    wrapProgram $out/bin/ignis \
+      --set PATH "${pkgs.gst_all_1.gstreamer}/bin:${pkgs.dart-sass}/bin:$PATH" \
+      --set PYTHONPATH "${concatStringsSep ":" (map (pkg: "${pkg}/lib/python3.12/site-packages") [
+        pkgs.python312Packages.materialyoucolor
+        pkgs.python312Packages.pillow
+        pkgs.python312Packages.markupsafe
+        pkgs.python312Packages.jinja2
+        pkgs.python312Packages.gst-python
+        pkgs.python312Packages.pygobject3
+        pkgs.python312Packages.pycairo
+        pkgs.python312Packages.loguru
+        pkgs.python312Packages.certifi
+        pkgs.python312Packages.idna
+        pkgs.python312Packages.urllib3
+        pkgs.python312Packages.requests
+        pkgs.python312Packages.click
+        pkgs.python312Packages.charset-normalizer
+      ])}:$out/lib/python3.12/site-packages:$PYTHONPATH" \
+      --set GI_TYPELIB_PATH "$out/lib:${concatStringsSep ":" (map (pkg: "${pkg}/lib/girepository-1.0") [
+        pkgs.glib
+        pkgs.gobject-introspection
+        pkgs.networkmanager
+        pkgs.gobject-introspection-unwrapped
+        pkgs.gst_all_1.gstreamer
+        pkgs.gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-ugly
+      ])}:$GI_TYPELIB_PATH" \
+      --set LD_LIBRARY_PATH "$out/lib:${pkgs.gtk4-layer-shell}/lib:${pkgs.glib}/lib:$LD_LIBRARY_PATH" \
+      --set GST_PLUGIN_PATH "${concatStringsSep ":" (map (pkg: "${pkg}/lib/gstreamer-1.0") [
+        pkgs.gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-ugly
+      ])}:$GST_PLUGIN_PATH"
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Full-featured Python framework for building desktop shells using GTK4";
+    homepage = "https://github.com/linkfrg/ignis";
+    changelog = "https://github.com/linkfrg/ignis/releases/tag/v${version}";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ frdiener somokill ];
+    main_program = "ignis";
+  };
+}
