8510c5eb20418d20c6fb50c5b5ff4dad47eb2015
linkfrg <link071209@gmail.com>
2024-10-08 22:43:20 +0500

    feat: add support for custom paths and xml in Utils.load_interface_xml()

--- ignis/utils/load_interface_xml.py
+++ ignis/utils/load_interface_xml.py
@@ -4,17 +4,36 @@ from .get_current_dir import get_current_dir
 DBUS_DIR = get_current_dir() + "/../dbus"
 
 
-def load_interface_xml(interface_name: str) -> Gio.DBusInterfaceInfo:
+def load_interface_xml(
+    interface_name: str | None = None, path: str | None = None, xml: str | None = None
+) -> Gio.DBusInterfaceInfo:
     """
     Load interface info from XML.
-    Interfaces must be stored in the ``ignis/dbus/`` directory.
+    If you want to load interface info from the path or XML string, you need to provide ``path`` and ``xml`` as keyword arguments respectively.
 
     Args:
-        interface_name (``str``): The name of the interface.
+        interface_name (``str | None``, optional): The name of the interface. The interface must be stored in the ``ignis/dbus/`` directory in the Ignis sources.
+        path (``str | None``, optional): The full path to the interface XML.
+        xml (``str | None``, optional): The XML string.
+    Raises:
+        TypeError: If neither of the arguments is provided.
     Returns:
         ``Gio.DBusInterfaceInfo``: The interface information.
     """
-    file_path = f"{DBUS_DIR}/{interface_name}.xml"
-    with open(file_path) as file:
-        xml = file.read()
-    return Gio.DBusNodeInfo.new_for_xml(xml).interfaces[0]
+    xml_string: str
+
+    if interface_name:
+        file_path = f"{DBUS_DIR}/{interface_name}.xml"
+        with open(file_path) as file:
+            xml_string = file.read()
+    elif path:
+        with open(path) as file:
+            xml_string = file.read()
+    elif xml:
+        xml_string = xml
+    else:
+        raise TypeError(
+            "load_interface_xml() requires at least one positional argument"
+        )
+
+    return Gio.DBusNodeInfo.new_for_xml(xml_string).interfaces[0]
