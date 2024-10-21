760ccdba964fe1b27d6a9db2d8399d920514e97b
linkfrg <link071209@gmail.com>
2024-10-21 22:25:09 +0500

    upower: move to D-Bus; nuke UPowerGlib

--- ignis/services/upower/device.py
+++ ignis/services/upower/device.py
@@ -1,6 +1,8 @@
 from ignis.gobject import IgnisGObject
-from ._imports import UPowerGlib
-from gi.repository import GObject  # type: ignore
+from gi.repository import GObject, GLib  # type: ignore
+from ignis.dbus import DBusProxy
+from ignis.utils import Utils
+from .constants import DEVICE_KIND, DeviceState
 
 
 class UPowerDevice(IgnisGObject):
@@ -11,7 +13,8 @@ class UPowerDevice(IgnisGObject):
         - **removed** (): Emitted when the device has been removed.
 
     Properties:
-        - **device** (``UPowerGlib.Device``, read-only): The instance of ``UPowerGlib.Device``.
+        - **object_path** (``str``, read-only): The D-Bus object path of the device.
+        - **proxy** (:class:`~ignis.dbus.DBusProxy`, read-only): The instance of :class:`~ignis.dbus.DBusProxy` for this device.
         - **native_path** (``str``, read-only): The native path of the device.
         - **kind** (``str``, read-only): The device kind, e.g. ``"battery"``.
         - **available** (``bool`` read-only): Whether the device is available.
@@ -38,125 +41,130 @@ class UPowerDevice(IgnisGObject):
         "removed": (GObject.SignalFlags.RUN_FIRST, GObject.TYPE_NONE, ()),
     }
 
-    def __init__(self, device: UPowerGlib.Device):
+    def __init__(self, object_path: str):
         super().__init__()
 
-        self._device = device
+        self.__watching_props: dict[str, tuple[str, ...]] = {}
+        self._object_path = object_path
 
-        self._charge_threshold: bool = False
-        self._charge_threshold_supported: bool = False
-        self._charge_start_threshold: int = 0
-        self._charge_end_threshold: int = 0
-
-        for prop_name in [
-            "icon-name",
-            "energy",
-            "energy-full",
-            "energy-rate",
-            "charge-cycles",
-            "power-supply",
-            "voltage",
-            "temperature",
-        ]:
-            self.__conn_dev_notif(prop_name, prop_name)
-
-        self.__conn_dev_notif("is-present", "available")
-        self.__conn_dev_notif("percentage", "percent")
-        self.__conn_dev_notif("state", "charging", "charged")
-
-        self.__conn_dev_notif("time-to-full", "time-remaining")
-        self.__conn_dev_notif("state", "time-remaining")
-        self.__conn_dev_notif("time-to-empty", "time-remaining")
-
-    def __conn_dev_notif(self, dev_prop: str, *self_props: str) -> None:
-        self._device.connect(
-            f"notify::{dev_prop}", lambda x, y: (self.notify(i) for i in self_props)
+        self._proxy = DBusProxy(
+            name="org.freedesktop.UPower",
+            object_path=object_path,
+            interface_name="org.freedesktop.UPower.Device",
+            info=Utils.load_interface_xml("org.freedesktop.UPower.Device"),
+            bus_type="system",
         )
+        self._proxy.proxy.connect("g-properties-changed", self.__sync)
+
+        self.__watch_property("Percentage", "percent")
+        self.__watch_property("Energy", "energy")
+        self.__watch_property("EnergyFull", "energy-full")
+        self.__watch_property("EnergyRate", "energy-rate")
+        self.__watch_property("ChargeCycles", "charge-cycles")
+        self.__watch_property("PowerSupply", "power-supply")
+        self.__watch_property("Voltage", "voltage")
+        self.__watch_property("Temperature", "temperature")
+
+        self.__watch_property("IsPresent", "available")
+        self.__watch_property("State", "charging", "charged", "time-remaining")
+        self.__watch_property("TimeToFull", "time-remaining")
+        self.__watch_property("TimeToEmpty", "time-remaining")
+
+    def __watch_property(self, dbus_property: str, *prop_names: str) -> None:
+        self.__watching_props[dbus_property] = prop_names
+
+    def __sync(self, proxy, properties: GLib.Variant, invalidated_properties) -> None:
+        prop_dict = properties.unpack()
+
+        for dbus_property in prop_dict.keys():
+            if dbus_property in self.__watching_props:
+                for i in self.__watching_props[dbus_property]:
+                    self.notify(i)
 
     @GObject.Property
-    def device(self) -> UPowerGlib.Device:
-        return self._device
+    def object_path(self) -> str:
+        return self._object_path
+
+    @GObject.Property
+    def proxy(self) -> DBusProxy:
+        return self._proxy
 
     @GObject.Property
     def native_path(self) -> str:
-        return self._device.props.native_path
+        return self._proxy.NativePath
 
     @GObject.Property
     def kind(self) -> str:
-        return UPowerGlib.Device.kind_to_string(self._device.props.kind)  # type: ignore
+        return DEVICE_KIND.get(self._proxy.Type, "unknown")
 
     @GObject.Property
     def available(self) -> bool:
-        return self._device.props.is_present
+        return self._proxy.IsPresent
 
     @GObject.Property
     def percent(self) -> float:
-        return self._device.props.percentage
+        return self._proxy.Percentage
 
     @GObject.Property
     def charging(self) -> bool:
-        return self._device.props.state == UPowerGlib.DeviceState.CHARGING
+        return self._proxy.State == DeviceState.CHARGING
 
     @GObject.Property
     def charged(self) -> bool:
-        return self._device.props.state == UPowerGlib.DeviceState.FULLY_CHARGED
+        return self._proxy.State == DeviceState.FULLY_CHARGED
 
     @GObject.Property
     def icon_name(self) -> str:
-        return self._device.props.icon_name
+        return self._proxy.IconName
 
     @GObject.Property
     def time_remaining(self) -> int:
-        return (
-            self._device.props.time_to_full
-            if self.charging
-            else self._device.props.time_to_empty
-        )
+        return self._proxy.TimeToFull if self.charging else self._proxy.TimeToEmpty
 
     @GObject.Property
     def energy(self) -> float:
-        return self._device.props.energy
+        return self._proxy.Energy
 
     @GObject.Property
     def energy_full(self) -> float:
-        return self._device.props.energy_full
+        return self._proxy.EnergyFull
 
     @GObject.Property
     def energy_full_design(self) -> float:
-        return self._device.props.energy_full_design
+        return self._proxy.EnergyDesign
 
     @GObject.Property
     def energy_rate(self) -> float:
-        return self._device.props.energy_rate
+        return self._proxy.EnergyRate
 
     @GObject.Property
     def charge_cycles(self) -> int:
-        return self._device.props.charge_cycles
+        return self._proxy.ChargeCycles
 
     @GObject.Property
     def vendor(self) -> str:
-        return self._device.props.vendor
+        return self._proxy.Vendor
 
     @GObject.Property
     def model(self) -> str:
-        return self._device.props.model
+        return self._proxy.Model
 
     @GObject.Property
     def serial(self) -> str:
-        return self._device.props.serial
+        return self._proxy.Serial
 
     @GObject.Property
     def power_supply(self) -> bool:
-        return self._device.props.power_supply
+        return self._proxy.PowerSupply
 
     @GObject.Property
     def technology(self) -> str:
-        return UPowerGlib.Device.technology_to_string(self._device.props.technology)  # type: ignore
+        return DEVICE_KIND.get(self._proxy.Technology, "unknown")
 
     @GObject.Property
     def temperature(self) -> float:
-        return self._device.props.temperature
+        return self._proxy.Temperature
 
     @GObject.Property
     def voltage(self) -> float:
-        return self._device.props.voltage
+        return self._proxy.Voltage
--- ignis/services/upower/service.py
+++ ignis/services/upower/service.py
@@ -1,6 +1,8 @@
 from ignis.base_service import BaseService
 from gi.repository import GObject  # type: ignore
-from ._imports import UPowerGlib
+from ignis.dbus import DBusProxy
+from ignis.utils import Utils
+from ignis.exceptions import UPowerNotRunningError
 from .device import UPowerDevice
 
 
@@ -35,16 +37,35 @@ class UPowerService(BaseService):
     def __init__(self) -> None:
         super().__init__()
 
-        self._client = UPowerGlib.Client.new()
+        self._proxy = DBusProxy(
+            name="org.freedesktop.UPower",
+            object_path="/org/freedesktop/UPower",
+            interface_name="org.freedesktop.UPower",
+            info=Utils.load_interface_xml("org.freedesktop.UPower"),
+            bus_type="system",
+        )
+
+        if not self._proxy.has_owner:
+            raise UPowerNotRunningError()
+
         self._devices: dict[str, UPowerDevice] = {}
         self._batteries: dict[str, UPowerDevice] = {}
-        self._display_device = UPowerDevice(device=self._client.get_display_device())
+        self._display_device = UPowerDevice(object_path=self._proxy.GetDisplayDevice())
 
-        self._client.connect("device-added", self.__add_device)
-        self._client.connect("device-removed", self.__remove_device)
+        self._proxy.signal_subscribe(
+            "DeviceAdded",
+            lambda *args: self.__add_device(self.__get_device_object_path(args)),
+        )
+        self._proxy.signal_subscribe(
+            "DeviceRemoved",
+            lambda *args: self.__remove_device(self.__get_device_object_path(args)),
+        )
 
-        for device in self._client.get_devices():
-            self.__add_device(None, device)
+        for device in self._proxy.EnumerateDevices():
+            self.__add_device(device)
+
+    def __get_device_object_path(self, args) -> str:
+        return args[-1].unpack()[0]  # -1 element is the device object path (Variant)
 
     @GObject.Property
     def devices(self) -> list[UPowerDevice]:
@@ -58,16 +79,16 @@ class UPowerService(BaseService):
     def display_device(self) -> UPowerDevice:
         return self._display_device
 
-    def __add_device(self, x, gdevice: UPowerGlib.Device) -> None:
-        device = UPowerDevice(device=gdevice)
-        self._devices[gdevice.get_object_path()] = device
+    def __add_device(self, object_path: str) -> None:
+        device = UPowerDevice(object_path=object_path)
+        self._devices[object_path] = device
         self.emit("device-added", device)
 
-        if gdevice.props.kind == UPowerGlib.DeviceKind.BATTERY:
-            self._batteries[gdevice.get_object_path()] = device
+        if device.kind == "battery":
+            self._batteries[object_path] = device
             self.emit("battery-added", device)
 
-    def __remove_device(self, x, object_path: str) -> None:
+    def __remove_device(self, object_path: str) -> None:
         if object_path not in self._devices:
             return
 
