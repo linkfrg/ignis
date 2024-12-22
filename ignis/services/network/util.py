3a02e5984aa74786af9f2b4a3a5857fe34e1c5c7
Link <127321188+linkfrg@users.noreply.github.com>
2024-12-22 18:26:05 +0500

    nuke services.network.util.get_devices()

--- ignis/services/network/util.py
+++ ignis/services/network/util.py
@@ -1,15 +1,3 @@
-from collections.abc import Generator
-from ._imports import NM
-
-
-def get_devices(
-    client: NM.Client, device_type: NM.DeviceType
-) -> Generator[NM.Device, None, None]:
-    for d in client.get_devices():
-        if d.get_device_type() == device_type:
-            yield d
-
-
 def check_is_vpn(func):
     def wrapper(*args, **kwargs):
         connection = args[2]
