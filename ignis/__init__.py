9afd1217bacbbfe46a5644b7483b269495b42d1a
Link <127321188+linkfrg@users.noreply.github.com>
2024-12-28 20:17:03 +0500

    fix: store libgvc in the separate wheel directory

--- ignis/__init__.py
+++ ignis/__init__.py
@@ -7,6 +7,7 @@ from gi.repository import GLib  # type: ignore
 __version__ = "0.3.dev0"
 __lib_dir__ = None
 CACHE_DIR = None
+LIBDIR_NAME = ".ignis.mesonpy.libs"
 
 if "sphinx" not in sys.modules:
     CACHE_DIR = f"{GLib.get_user_cache_dir()}/ignis"
@@ -28,9 +29,12 @@ gi.require_version("GIRepository", "2.0")
 try:
     from gi.repository import GIRepository  # type: ignore
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 
-    GIRepository.Repository.prepend_library_path(current_directory)
-    GIRepository.Repository.prepend_search_path(current_directory)
+    libdir_path = os.path.join(parent_dir, LIBDIR_NAME)
+
+    GIRepository.Repository.prepend_library_path(libdir_path)
+    GIRepository.Repository.prepend_search_path(libdir_path)
 except TypeError:
     pass
