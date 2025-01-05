8c56a4558124ce1feab7c9e8d5c0c0d4e804d48e
Link <127321188+linkfrg@users.noreply.github.com>
2025-01-06 00:44:41 +0500

    feat: new signal `OptionsGroup.subgroup_changed`; add more docstrings

--- ignis/options_manager.py
+++ ignis/options_manager.py
@@ -7,6 +7,13 @@ from collections.abc import Generator
 
 
 class Option(IgnisGObject):
+    """
+    :meta private:
+
+    The class for simulating a GObject that represents an option and its value,
+    need for OptionsGroup.bind()
+    """
+
     def __init__(self, manager: "OptionsGroup", name: str):
         super().__init__()
         self._manager = manager
@@ -24,15 +31,39 @@ class Option(IgnisGObject):
 
 
 class OptionsGroup(IgnisGObject):
+    """
+    An options group.
+    """
     def __init__(self):
         super().__init__()
         self._modified_options: dict[str, Any] = {}
 
-        for name, manager in self.__yield_subgroups():
-            manager.connect("changed", lambda x, name: self.emit("changed", name))
+        for subgroup_name, subgroup in self.__yield_subgroups():
+            subgroup.connect(
+                "changed",
+                lambda x, option_name, subgroup_name=subgroup_name: self.emit(
+                    "subgroup-changed", subgroup_name, option_name
+                ),
+            )
 
     @GObject.Signal(arg_types=(str,))
-    def changed(self, *args): ...
+    def changed(self, *args):
+        """
+        Emitted when an option of this group has changed
+
+        Args:
+            option_name: The name of the option.
+        """
+
+    @GObject.Signal(arg_types=(str, str))
+    def subgroup_changed(self, *args):
+        """
+        Emitted when an option of a subgroup has changed
+
+        Args:
+            subgroup_name: The name of the subgroup.
+            option_name: The name of the option.
+        """
 
     def __setattr__(self, name: str, value: Any, emit: bool = True) -> None:
         if not name.startswith("_"):
@@ -41,11 +72,32 @@ class OptionsGroup(IgnisGObject):
                 self.emit("changed", name)
         return super().__setattr__(name, value)
 
+    def __getattribute__(self, name: str) -> Any:
+        if name.startswith("set_"):
+            property_name = name.replace("set_", "")
+            if hasattr(self, property_name):
+                return lambda value: setattr(self, property_name, value)
+        elif name.startswith("get_"):
+            property_name = name.replace("get_", "")
+            if hasattr(self, property_name):
+                return lambda: getattr(self, property_name)
+
+        return super().__getattribute__(name)
+
     def bind(self, property_name: str, transform: Callable | None = None) -> Binding:
+        """
+        :meta private:
+
+        Creates a fake GObject that represents an option
+        and makes Binding for it
+        """
         opt_obj = Option(self, property_name)
         return Binding(opt_obj, "value", transform)
 
     def to_dict(self) -> dict:
+        """
+        Returns a dictionary representation of all options and subgroups.
+        """
         data = self._modified_options.copy()
         for name, manager in self.__yield_subgroups():
             data[name] = manager.to_dict()
@@ -53,6 +105,12 @@ class OptionsGroup(IgnisGObject):
         return data
 
     def apply_from_dict(self, data: dict) -> None:
+        """
+        Apply values for options from a dict.
+
+        Args:
+            data: A dict with values to apply.
+        """
         for key, value in data.items():
             if not hasattr(self, key):
                 continue
@@ -70,25 +128,44 @@ class OptionsGroup(IgnisGObject):
             if isinstance(value, OptionsGroup):
                 yield key, value
 
-    def __getattribute__(self, name: str) -> Any:
-        if name.startswith("set_"):
-            property_name = name.replace("set_", "")
-            if hasattr(self, property_name):
-                return lambda value: setattr(self, property_name, value)
-        elif name.startswith("get_"):
-            property_name = name.replace("get_", "")
-            if hasattr(self, property_name):
-                return lambda: getattr(self, property_name)
-
-        return super().__getattribute__(name)
-
 
 class OptionsManager(OptionsGroup):
+    """
+    Bases: ``~ignis.options_manager.OptionsGroup``.
+
+    This is the top-level class in the options hierarchy.
+
+    The common options hierarchy must look like this:
+
+    .. code-block:: python
+
+        from ignis.options_manager import OptionsManager, OptionsGroup
+
+        class Options(OptionsManager):
+            def __init__(self):
+                super().__init__(file="PATH/TO/FILE")
+
+            class Subgroup1(OptionsGroup):
+                option1: bool = False
+                option2: int = 5000
+
+            class SomeSubgroup(OptionsGroup):
+                example_option: str | None = get_something...()
+                test: str = "%Y-%m-%d_%H-%M-%S.mp4"
+
+
+            subgroup1 = Subgroup1()
+            some_subgroup = SomeSubgroup()
+
+        options = Options()
+
+    """
     def __init__(self, file: str):
         super().__init__()
         self._file = file
         if "sphinx" not in sys.modules:
             self.connect("changed", self.__dump)
+            self.connect("subgroup-changed", self.__dump)
             try:
                 self.load(self._file)
             except json.decoder.JSONDecodeError:
