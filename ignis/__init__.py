2883d44bcd44d6ea7acdfa160be1a26f8f873e63
Link <127321188+linkfrg@users.noreply.github.com>
2025-03-16 19:54:52 +0500

    fix: resolve compatibility issues with PyGObject 3.52.0 (girepository 2.0) (#202)

--- ignis/__init__.py
+++ ignis/__init__.py
@@ -14,6 +14,12 @@ CACHE_DIR = None
 
 is_sphinx_build: bool = "sphinx" in sys.modules
 is_editable_install: bool = False
+is_girepository_2_0: bool
+
+if not is_sphinx_build:
+    is_girepository_2_0 = gi.version_info >= (3, 51, 0)  # type: ignore
+else:
+    is_girepository_2_0 = False
 
 try:
     direct_url = Distribution.from_name("ignis").read_text("direct_url.json")
@@ -43,17 +49,28 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Gdk", "4.0")
 gi.require_version("Gtk4LayerShell", "1.0")
 gi.require_version("GdkPixbuf", "2.0")
-gi.require_version("GIRepository", "2.0")
-
-from gi.repository import GIRepository  # type: ignore  # noqa: E402
 
 
 def prepend_to_repo(path: str) -> None:
-    try:
-        GIRepository.Repository.prepend_library_path(path)
-        GIRepository.Repository.prepend_search_path(path)
-    except TypeError:
-        pass
+    if is_girepository_2_0:
+        # Hacky? yes
+        # But getting GIRepository from gi.repository and using its prepend methods results in no effect
+        # So, we import Repository from _gi, which is used by the gi module itself (you can see that in gi/__init__.py)
+        from gi._gi import Repository  # type: ignore # noqa: E402
+
+        # Original GIRepository.Repository 3.0 doesn't have get_default()
+        # But! This Repository magically have it, idk why
+        # And you must call it, otherwise (using prepend methods as class methods) an error will be thrown
+        # See how gi.require_version() works internally
+        repo = Repository.get_default()
+    else:
+        gi.require_version("GIRepository", "2.0")
+        from gi.repository import GIRepository  # type: ignore
+
+        repo = GIRepository.Repository
+
+    repo.prepend_library_path(path)
+    repo.prepend_search_path(path)
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
