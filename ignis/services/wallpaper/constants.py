a0acd808395b47480576be65830323b9aaf9f7c1
Link <linkfrg.dev@proton.me>
2025-03-29 02:31:13 +0500

    feat!(`WallpaperService`): store cached wallpaper in `XDG_DATA_HOME`

--- ignis/services/wallpaper/constants.py
+++ ignis/services/wallpaper/constants.py
@@ -1,3 +1,17 @@
+import os
 import ignis
+import shutil
+from loguru import logger
 
-CACHE_WALLPAPER_PATH = f"{ignis.CACHE_DIR}/wallpaper"
+
+_OLD_CACHE_WALLPAPER_PATH = f"{ignis.CACHE_DIR}/wallpaper"
+CACHE_WALLPAPER_PATH = f"{ignis.DATA_DIR}/wallpaper"
+
+# FIXME: remove after v0.6 release
+if not os.path.exists(CACHE_WALLPAPER_PATH) and os.path.exists(
+    _OLD_CACHE_WALLPAPER_PATH
+):
+    logger.warning(
+        f"Copying the cached wallpaper to the new directory: {_OLD_CACHE_WALLPAPER_PATH} -> {CACHE_WALLPAPER_PATH}"
+    )
+    shutil.copy(_OLD_CACHE_WALLPAPER_PATH, CACHE_WALLPAPER_PATH)
