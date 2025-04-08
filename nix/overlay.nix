375a7a3e7b8dfa877478d98516c4351481a3865d
MOIS3Y <stepan@zhukovsky.me>
2025-04-09 01:55:33 +0900

    nix: move gvc to flake input, explicit input args for the build module

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,3 +1,3 @@
-{ self, version }: final: prev: {
-  ignis = prev.callPackage ./ignis.nix { inherit self; inherit version; };
+{ self, version, gvc }: final: prev: {
+  ignis = prev.callPackage ./ignis.nix { inherit self gvc version; };
 }
